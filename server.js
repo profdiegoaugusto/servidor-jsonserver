b8ea496b25260d628d1cf2c16b9b297da4ab36f4
profdiegoaugusto <diaugustobarros@gmail.com>
2024-06-14 08:09:58 -0300

    post bug fix

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('/tmp/db.json')
+
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('data/db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
