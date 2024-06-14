a97dac7effbee3867d0d9f607524eb95957017a5
profdiegoaugusto <diaugustobarros@gmail.com>
2024-06-14 08:06:30 -0300

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('tmp/db.json')
+const router = jsonServer.router('/tmp/db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
